e82dea94607bb2b1df81420b2256dc97e73139e8
Matheus Palmieri <matheuspalmireidev@gmail.com>
2025-07-29 13:29:59 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,13 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [react(), dts()],
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "UiLib",
+      name: "ReactUiLib",
       fileName: (format) => `react-ui-lib.${format}.js`,
-      formats: ["es", "cjs"], // output: ESM e CommonJS
+      formats: ["es", "cjs"],
     },
     rollupOptions: {
       external: ["react", "react-dom"],
