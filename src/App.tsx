92da74c3165ee6ff8f9373458aac16a1e0a8eefe
Matheus Palmieri <matheuspalmireidev@gmail.com>
2025-07-29 11:40:15 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  return (
+    <div className="bg-neutral-100 h-screen w-full">
+      <h1 className="text-2xl font-bold">Hello World</h1>
+
+      <Button>Click me</Button>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts({ insertTypesEntry: true })],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ReactJumpadUI",
+      formats: ["es", "umd"],
+      fileName: (format) => `react-jumpad-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
